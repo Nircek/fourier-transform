ebfbc41c3a60bf17dafa2526e93d8e36f718d10b
Marcin Zepp <nircek-2103@protonmail.com>
2021-06-13 16:07:41 +0200

    Add oscilloscope class and avg()

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 // @ts-check
-/** @type {HTMLCollectionOf<HTMLCanvasElement>} */ var ss;
-/** @type {HTMLCollectionOf<HTMLCanvasElement>} */ var cs;
+/** @type {HTMLElement} */ var ss;
+/** @type {HTMLElement[]} */ var cs = [];
 /** @type {number} */ var rate;
 /** @type {number?} */ var raf;
 
 function init() {
+    var osc = document.getElementsByClassName("oscilloscope")[0];
+    for (let i = 0; i < 3; ++i)
+        osc.parentElement.appendChild(osc.cloneNode(true));
     // @ts-ignore
-    ss = document.getElementsByClassName("canvas-sum");
-    // @ts-ignore
-    cs = document.getElementsByClassName("canvas-part");
+    cs = [...document.getElementsByClassName("oscilloscope")];
+    ss = cs.splice(0, 1)[0];
+    ss.getElementsByClassName("controls")[0].remove();
     refresh();
 }
 
 function refresh() {
-    rate = ss[0].offsetWidth;
-    for (let c of [...ss, ...cs])
-        c.width = rate;
+    rate = canvas(ss).offsetWidth;
+    for (let c of [ss, ...cs])
+        canvas(c).width = rate;
     if (raf !== undefined)
         cancelAnimationFrame(raf);
     raf = requestAnimationFrame(animate);
+}
+
+/**
+ * @param {HTMLElement} e
+ */
+function canvas(e) {
+    return e.getElementsByTagName('canvas')[0];
 }
 
 /**
@@ -38,37 +48,40 @@ function clear(w) {
 /**
  * @param {HTMLCanvasElement} w
  * @param {number[]} x
+ * @param {string | CanvasGradient | CanvasPattern} s
  */
-function render(w, x) {
+function render(w, x, s = "black") {
     const ctx = w.getContext("2d");
+    ctx.strokeStyle = s;
     ctx.beginPath();
+    const margin = 8;
+    /** @param {number} e */
+    const off = e => margin + (w.height / 2 - margin) * (1 - e);
     if (x.length)
-        ctx.moveTo(0, w.height / 2 * (1 - x[0]));
+        ctx.moveTo(0, off(x[0]));
     for (const [i, e] of x.entries())
-        ctx.lineTo(i, w.height / 2 * (1 - e));
+        ctx.lineTo(i, off(e));
     ctx.stroke();
 }
 
 /**
- * @param {number[]} x
- * @param {number[]} y
+ * @param {number[][]} args
  */
-function add(x, y) {
-    console.assert(x.length == y.length);
-    return x.map((e, i) => e + y[i]);
+function avg(...args) {
+    return args[0].map((_, i) => args.map(e => e[i]).reduce((p, n) => p + n, 0) / args.length);
 };
 
 function animate() {
-    let a = sin(.1, .333 * Math.sin(Date.now() / 1e3), Date.now() / 1e4)
-    let b = sin(.2 + .1 * Math.sin(Date.now() / 1e4), .333, Date.now() / 1e3);
-    let c = sin(1 / Math.PI, .333, Date.now() / 5e3);
-    let s = add(add(a, b), c);
-    for (let c of [...ss, ...cs])
-        clear(c);
-    render(ss[0], s);
-    render(cs[0], a);
-    render(cs[1], b);
-    render(cs[2], c);
+    let a = sin(.1, Math.sin(Date.now() / 1e3), Date.now() / 1e4)
+    let b = sin(.2 + .1 * Math.sin(Date.now() / 1e4), 1, Date.now() / 1e3);
+    let c = sin(1 / Math.PI, 1, Date.now() / 5e3);
+    let s = avg(a, b, c);
+    for (let c of [ss, ...cs])
+        clear(canvas(c));
+    render(canvas(ss), s);
+    render(canvas(cs[0]), a);
+    render(canvas(cs[1]), b);
+    render(canvas(cs[2]), c);
     raf = requestAnimationFrame(animate);
 }
 window.addEventListener('load', init);
