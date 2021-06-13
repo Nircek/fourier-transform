1fdf050f64fd7072a18236253f04114e2d26cc72
Marcin Zepp <nircek-2103@protonmail.com>
2021-06-13 13:57:06 +0200

    Add style, refactor and fix bugs

--- script.js
+++ script.js
@@ -1,39 +1,54 @@
 // @ts-check
-/** @type {HTMLCollectionOf<HTMLCanvasElement>} */
-var ss;
-/** @type {HTMLCollectionOf<HTMLCanvasElement>} */
-var cs;
-var rate, max;
+/** @type {HTMLCollectionOf<HTMLCanvasElement>} */ var ss;
+/** @type {HTMLCollectionOf<HTMLCanvasElement>} */ var cs;
+/** @type {number} */ var rate;
+/** @type {number?} */ var raf;
+
 function init() {
     // @ts-ignore
     ss = document.getElementsByClassName("canvas-sum");
     // @ts-ignore
     cs = document.getElementsByClassName("canvas-part");
-    rate = window.innerWidth - 18;
-    max = (window.innerHeight / 4 - 10);
-    requestAnimationFrame(animate);
+    refresh();
 }
+
+function refresh() {
+    rate = ss[0].offsetWidth;
+    for (let c of [...ss, ...cs])
+        c.width = rate;
+    if (raf !== undefined)
+        cancelAnimationFrame(raf);
+    raf = requestAnimationFrame(animate);
+}
+
 /**
  * @param {number} f
- * @param {number} a
  */
-function sin(f, a, s = 0) {
+function sin(f, a = 1, s = 0) {
     return Array.from(Array(rate), (_, i) => a * Math.sin(s * 2 * Math.PI + i * f));
 };
+
+/**
+ * @param {HTMLCanvasElement} w
+ */
+function clear(w) {
+    w.getContext("2d").clearRect(0, 0, w.width, w.height);
+}
+
 /**
  * @param {HTMLCanvasElement} w
  * @param {number[]} x
  */
 function render(w, x) {
-    w.width = rate;
-    w.height = max;
     const ctx = w.getContext("2d");
+    ctx.beginPath();
     if (x.length)
-        ctx.moveTo(0, x[0]);
+        ctx.moveTo(0, w.height / 2 * (1 - x[0]));
     for (const [i, e] of x.entries())
-        ctx.lineTo(i, max / 2 * (1 - e));
+        ctx.lineTo(i, w.height / 2 * (1 - e));
     ctx.stroke();
 }
+
 /**
  * @param {number[]} x
  * @param {number[]} y
@@ -48,11 +63,13 @@ function animate() {
     let b = sin(.2 + .1 * Math.sin(Date.now() / 1e4), .333, Date.now() / 1e3);
     let c = sin(1 / Math.PI, .333, Date.now() / 5e3);
     let s = add(add(a, b), c);
+    for (let c of [...ss, ...cs])
+        clear(c);
     render(ss[0], s);
     render(cs[0], a);
     render(cs[1], b);
     render(cs[2], c);
-    requestAnimationFrame(animate);
+    raf = requestAnimationFrame(animate);
 }
 window.addEventListener('load', init);
-window.addEventListener('resize', init);
+window.addEventListener('resize', refresh);
